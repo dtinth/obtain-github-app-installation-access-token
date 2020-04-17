65588411d109d0cd6336b885e60dc5cda50c63f6
Thai Pangsakulyanont <dtinth@spacet.me>
2020-04-17 19:14:52 +0700

    Add a version for CI

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { readFileSync } from 'fs'
 cli()
   .command(
     '$0',
-    'Obtains a GitHub App Installation Token',
+    'Obtains a GitHub App Installation Token from command-line arguments',
     {
       appId: {
         alias: 'a',
@@ -26,13 +26,36 @@ cli()
         demand: true,
       },
     },
-    async args => {
+    async (args) => {
       const app = new App({
         id: args.appId,
         privateKey: readFileSync(args.privateKey, 'utf8'),
       })
       const installationAccessToken = await app.getInstallationAccessToken({
         installationId: args.installationId,
+      })
+      console.log(installationAccessToken)
+    },
+  )
+  .command(
+    'ci <token>',
+    'Obtains a GitHub App Installation Token from Base64-encoded credentials token',
+    {
+      token: {
+        description:
+          'Base64-encoded of JSON object with { appId, installationId, privateKey }',
+        type: 'string',
+        demand: true,
+      },
+    },
+    async (args) => {
+      const payload = JSON.parse(Buffer.from(args.token, 'base64').toString())
+      const app = new App({
+        id: +payload.appId,
+        privateKey: payload.privateKey,
+      })
+      const installationAccessToken = await app.getInstallationAccessToken({
+        installationId: +payload.installationId,
       })
       console.log(installationAccessToken)
     },
